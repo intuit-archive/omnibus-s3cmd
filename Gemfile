ebf96c7e432ea094b85411f6c683b163570cb000
ccloes <chad_cloes@intuit.com>
2013-05-15 09:38:22 -0700

    Initial checkin

---
+++ Gemfile
@@ -0,0 +1,4 @@
+source 'https://rubygems.org'
+
+gem 'omnibus', :git => 'https://github.com/opscode/omnibus-ruby.git', ref => '1.0.3'
+gem "omnibus-software", :git => "https://github.com/opscode/omnibus-software.git", :ref => '372133843737cbfaec270522d3207c4a2603a0de'
---
+++ config/software/intu-s3cmd.rb
@@ -0,0 +1,13 @@
+name "intu-s3cmd"
+version "1.5.0-alpha3"
+
+dependencies ["openssl", "python", "virtualenv"]
+
+source :url => "https://github.com/s3tools/s3cmd/archive/v1.5.0-alpha3.zip",
+       :md5 => "3aa1a6af0bc9ae46e39c2fd47f5a73f1"
+
+relative_path "s3cmd-1.5.0-alpha3"
+
+build do
+  command ". #{install_dir}/bin/activate; pip install --install-option='--prefix=#{install_dir}' s3cmd"
+end
